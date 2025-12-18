d490c0f9462129bb6936f8c8fbd483a890756b24
Roman Markov <krv.work@yandex.ru>
2025-12-18 13:31:51 +0300

    commit_18

--- main.go
+++ main.go
@@ -267,42 +267,45 @@ func (v *Validator) validateResources(node *yaml.Node, containerIndex int) {
 }
 
 func (v *Validator) validateResourceMap(node *yaml.Node, containerIndex int, mapType string) {
-	if node.Kind != yaml.MappingNode {
-		v.addError(node.Line, "spec.containers[%d].resources.%s must be an object", containerIndex, mapType)
-		return
-	}
+    if node.Kind != yaml.MappingNode {
+        v.addError(node.Line, "spec.containers[%d].resources.%s must be an object", containerIndex, mapType)
+        return
+    }
 
-	for i := 0; i < len(node.Content); i += 2 {
-		keyNode := node.Content[i]
-		valueNode := node.Content[i+1]
+    for i := 0; i < len(node.Content); i += 2 {
+        keyNode := node.Content[i]
+        valueNode := node.Content[i+1]
 
-		switch keyNode.Value {
-		case "cpu":
-    if valueNode.Kind != yaml.ScalarNode {
-        v.addError(valueNode.Line, "spec.containers[%d].resources.%s.cpu must be int", containerIndex, mapType)
-    } else if valueNode.Tag == "" || valueNode.Tag == "!!str" {
-        // Если tag пустой или это строка, выдаем ошибку
-        v.addError(valueNode.Line, "spec.containers[%d].resources.%s.cpu must be int", containerIndex, mapType)
-    } else if valueNode.Tag != "!!int" {
-        v.addError(valueNode.Line, "spec.containers[%d].resources.%s.cpu must be int", containerIndex, mapType)
-    } else {
-        cpu, err := strconv.Atoi(valueNode.Value)
-        if err != nil {
-            v.addError(valueNode.Line, "spec.containers[%d].resources.%s.cpu must be int", containerIndex, mapType)
-        } else if cpu <= 0 {
-            v.addError(valueNode.Line, "spec.containers[%d].resources.%s.cpu value out of range", containerIndex, mapType)
+        switch keyNode.Value {
+        case "cpu":
+            if valueNode.Kind != yaml.ScalarNode {
+                v.addError(valueNode.Line, "spec.containers[%d].resources.%s.cpu must be int", containerIndex, mapType)
+            } else {
+                // Ключевое исправление: проверяем что это именно int, а не строка
+                // В YAML: cpu: 2 (tag="!!int") - валидно
+                //         cpu: "2" (tag="!!str") - невалидно
+                if valueNode.Tag != "!!int" {
+                    v.addError(valueNode.Line, "spec.containers[%d].resources.%s.cpu must be int", containerIndex, mapType)
+                } else {
+                    // Если tag правильный, проверяем что это действительно число
+                    cpu, err := strconv.Atoi(valueNode.Value)
+                    if err != nil {
+                        v.addError(valueNode.Line, "spec.containers[%d].resources.%s.cpu must be int", containerIndex, mapType)
+                    } else if cpu <= 0 {
+                        v.addError(valueNode.Line, "spec.containers[%d].resources.%s.cpu value out of range", containerIndex, mapType)
+                    }
+                }
+            }
+        case "memory":
+            if valueNode.Kind != yaml.ScalarNode {
+                v.addError(valueNode.Line, "spec.containers[%d].resources.%s.memory must be string", containerIndex, mapType)
+            } else if !v.memoryRegex.MatchString(valueNode.Value) {
+                v.addError(valueNode.Line, "spec.containers[%d].resources.%s.memory has invalid format '%s'", containerIndex, mapType, valueNode.Value)
+            }
+        default:
+            v.addError(keyNode.Line, "spec.containers[%d].resources.%s.%s has unsupported resource type", containerIndex, mapType, keyNode.Value)
         }
     }
-		case "memory":
-			if valueNode.Kind != yaml.ScalarNode {
-				v.addError(valueNode.Line, "spec.containers[%d].resources.%s.memory must be string", containerIndex, mapType)
-			} else if !v.memoryRegex.MatchString(valueNode.Value) {
-				v.addError(valueNode.Line, "spec.containers[%d].resources.%s.memory has invalid format '%s'", containerIndex, mapType, valueNode.Value)
-			}
-		default:
-			v.addError(keyNode.Line, "spec.containers[%d].resources.%s.%s has unsupported resource type", containerIndex, mapType, keyNode.Value)
-		}
-	}
 }
 
 func (v *Validator) getField(node *yaml.Node, fieldName string) *yaml.Node {
